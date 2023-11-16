f2df84743fb37d2f7652e69bd1dad21753bad18b
HueHueberry <huby.new1112@gmail.com>
2023-11-16 08:53:29 +0800

    Fast image extraction algo added

--- modules/RTPSExtractor.py
+++ modules/RTPSExtractor.py
@@ -10,16 +10,23 @@ class PacketImageExtractor:
 	def __init__(self, packetInit: bytes, filename: str="yeet.jpeg"):
 		self.packetData = packetInit
 		self.name = filename
+		self.packetSize = 0
+		self.startIndex = 0
+		self.endIndex = 0
 
+	# get packet info during packet appending (faster)
 	def appendPacket(self, packet: bytes):
+		if (b"\xff\xd8" in packet): self.startIndex = self.packetSize + packet.rindex(b"\xff\xd8")
+		if (b"\xff\xd9" in packet): self.endIndex = self.packetSize + packet.rindex(b"\xff\xd9")
+		self.packetSize += len(packet)
 		self.packetData += packet
 
 	# retrieves the jpeg image buffer
 	def decodeImage(self):
-		startIndex = self.packetData.rindex(b"\xff\xd8")
-		endIndex = self.packetData.rindex(b"\xff\xd9")
-		dataBuffer = self.packetData[startIndex:endIndex + 2]
-		return dataBuffer
+		if (self.endIndex > self.startIndex):
+			dataBuffer = self.packetData[self.startIndex:self.endIndex + 2]
+			return dataBuffer
+		return b""
 
 	# saves image to specified filename
 	def extractImage(self):
